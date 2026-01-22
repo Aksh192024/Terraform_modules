ea36baa80884242500b797be5edb3dce58c54fe6
Aksh <Aksh1924@gmail.com>
2026-01-22 17:11:40 +0530

    update

--- vpc/variable.tf
+++ vpc/variable.tf
@@ -1,9 +1,9 @@
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+  description = "Enter the CIDR block for the VPC"
   type        = string
 }
 
 variable "vpc_name" {
-  description = "Name tag for the VPC"
+  description = "Enter the name of the VPC"
   type        = string
 }
